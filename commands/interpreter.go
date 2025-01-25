e70c02fd099959a753f623045276b9956a35c0f8
Arsen Hovhannisyan <arsen1994t@gmail.com>
2025-01-25 19:58:35 +0400

    Cleanup interpreter code.

--- commands/interpreter.go
+++ commands/interpreter.go
@@ -3,40 +3,33 @@ package commands
 import (
 	"bufio"
 	"fmt"
-	"gopasskeeper/actions"
+	"log"
 	"os"
 	"strings"
 )
 
-func GetCommandPrompt() (string, error) {
+func GetCommandPrompt() string {
 	for {
 		fmt.Print(">>> ")
 		reader := bufio.NewReader(os.Stdin)
 		enter, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal("something went wrong")
+		}
 		fmt.Print("\n")
 		enter = strings.TrimSpace(enter)
 
 		if enter == "" {
 			continue
 		}
-		return enter, err
+		return enter
 	}
-}
-
-func GetAction(command string) (*actions.Action, error) {
-	action, err := Validate(command)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return action, nil
 }
 
 func Run() {
 	for {
-		command, _ := GetCommandPrompt()
-		action, err := GetAction(command)
+		prompt := GetCommandPrompt()
+		action, err := Validate(prompt)
 		if err != nil {
 			fmt.Println(err)
 			continue
